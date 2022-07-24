7c7292178c104f1cee60e0e82dd2a592b69645fe
christo <christoslimit@gmail.com>
2022-07-24 15:18:29 +0530

    JS Array CheatSheet.

--- js-algo/src/App.js
+++ js-algo/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { CheatSheet } from './CheatSheet.js/CheatSheet';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Java Script Algorithms</h1>
+      <CheatSheet />
     </div>
   );
 }
--- js-algo/src/CheatSheet.js/CheatSheet.js
+++ js-algo/src/CheatSheet.js/CheatSheet.js
@@ -1,3 +1,96 @@
 export const CheatSheet = () => {
+    ArrayDemo();
+    return (
+        <>
+            <h3> Java Script Cheat Sheet </h3>
+        </>
+    )
+}
+
+const ArrayDemo = () => {
+
+    let fruits = ["Apple", "Banana", "Cherry"];
+    let fruit1 = [];
+
+    //Append
+    fruit1.push("Guava");
+    fruit1.push("Mango");
+    //Merge
+    //fruits = fruits.concat(fruit1);
+    fruits = [...fruits, ...fruit1];
+    console.log(fruits);
+
+    //Insert
+    fruits.splice(0, 0, "Avacado");
+    console.log(fruits);
+
+    //Remove
+    fruits.splice(0, 1);
+    console.log(fruits);
+
+    //find
+    let index = fruits.findIndex((element) => element === "Apple");
+    console.log(index);
+
+    //includes
+    if (fruits.includes("Apple")) {
+        console.log("Item is available.");
+    }
+
+    //slice
+    let fruits2 = fruits.slice(2);
+    console.log(fruits2);
+    let fruits3 = fruits.slice(-2);
+    console.log(fruits3);
+    let fruits4 = fruits.slice(1, 4); //1,2,3
+    console.log(fruits4);
+    console.log(fruits);
+
+    //iteration
+    for (let fruit of fruits) { console.log(fruit) }
+    for (let index in fruits) { console.log(index) }
+    for (let index = 0; index < fruits.length; index++) { console.log(index) }
+    let i = 0;
+    while (i < fruits.length) {
+        console.log(i)
+        i++;
+    }
+
+    fruits.forEach((fruit) => { console.log(fruit) });
+
+    let mapped = fruits.map((fruit) => {
+        return fruit + "s";
+    });
+    console.log(mapped);
+
+    let filtered = fruits.filter((fruit) => {
+        return fruit !== "Apple";
+    });
+    console.log(filtered)
+
+    let sorted = fruits.sort((a, b) => {
+        return b.charCodeAt(0) - a.charCodeAt(0);
+    })
+    console.log(sorted);
+
+    let reduced = fruits.reduce((par, val) => {
+        return par + val;
+    }, "")
+    console.log(reduced);
+}
+
+
+const TypeConversions = () => {
 
 }
+
+
+
+const StringDemo = () => {
+
+}
+
+const MapDemo = () => {
+
+}
+
